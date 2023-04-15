fa3a5e09ffc42f9cd6926d1922f95796b703f3f4
_Leg3ndary <ben.zhou.2006@gmail.com>
2023-04-14 22:50:49 -0400

    Learning le components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,12 @@
 import { Inter } from 'next/font/google'
+import NavBar from './components/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-0">
-      <nav className="flex items-center justify-between w-full bg-lime-500">
-        <a href="#" className="text-2xl font-bold">
-          MongoClient
-        </a>
-        <ul className="flex items-center space-x-4">
-          <li>
-            <a href="#" className="text-lg font-medium">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#" className="text-lg font-medium">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#" className="text-lg font-medium">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </main>
-  )
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-0">
+            <NavBar />
+        </main>
+    )
 }
