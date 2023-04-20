d8a5d8a4c31f80d61850642955fcf6e6c744b4eb
_Leg3ndary <ben.zhou.2006@gmail.com>
2023-04-19 22:21:54 -0400

    More updates

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,8 +2,9 @@ import NavBar from "@/components/NavBar";
 
 export default function About() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-0">
+        <main className="flex min-h-screen flex-col items-center p-0">
             <NavBar />
+            <h1 className="text-4xl font-bold pt-10">About</h1>
         </main>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-0">
+        <main className="flex min-h-screen flex-col items-center p-0">
             <NavBar />
         </main>
     );
