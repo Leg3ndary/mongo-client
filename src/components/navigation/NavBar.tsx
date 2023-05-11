ee103e5812713f64699aec362fa109dff86fd070
Ben <ben.zhou.2006@gmail.com>
2023-05-11 14:11:08 -0400

    Commit changes

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 
 const NavBar = () => {
     return (
-        <div className="navbar bg-gradient-to-r from-lime-300 via-lime-400 to-lime-500">
+        <div className="navbar bg-gradient-to-r from-lime-300 via-lime-400 to-lime-500 item-center">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">My Website</a>
+                <a className="btn btn-ghost normal-case text-xl navBarHeader">My Website</a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1 flex justify-end">
