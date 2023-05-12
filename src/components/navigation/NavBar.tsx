e05d9e853204954f9fc9d15940fb8aa4b789a48d
_Leg3ndary <ben.zhou.2006@gmail.com>
2023-05-11 22:24:11 -0400

    Update what I have so far

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -1,17 +1,16 @@
-// React component for the navigation bar
 import React from "react";
 import Link from "next/link";
 
 const NavBar = () => {
     return (
-        <div className="navbar bg-gradient-to-r from-lime-300 via-lime-400 to-lime-500 item-center">
+        <div className="navbar bg-gradient-to-r from-lime-300 via-lime-400 to-lime-500 flex items-center h-16">
             <div className="flex-1">
                 <a className="btn btn-ghost normal-case text-xl navBarHeader">My Website</a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1 flex justify-end">
                     <li>
-                        <Link className="" href="/">Home</Link>
+                        <Link href="/">Home</Link>
                     </li>
                     <li>
                         <Link href="/connect">Connect</Link>
